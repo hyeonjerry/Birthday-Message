4bd6ccf579e892dd81b92d5acbb90354c3835693
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2022-01-29 20:54:14 +0900

    FEAT - 메시지 목록 페이지 구현

--- web/urls.py
+++ web/urls.py
@@ -5,4 +5,5 @@ from web import views
 app_name = 'web'
 urlpatterns = [
     path('message/leave/', views.leave_message, name='leave_message'),
+    path('message/list/', views.message_list, name='message_list'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 
 from forms import MessageForm
+from web.models import Message
 
 
 def leave_message(request):
@@ -13,3 +14,9 @@ def leave_message(request):
         form = MessageForm()
     context = {'form': form}
     return render(request, 'leave_message.html', context)
+
+
+def message_list(request):
+    messages = Message.objects.order_by('-create_date')
+    context = {'messages': messages}
+    return render(request, 'message_list.html', context)

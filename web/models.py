5661a0b14108e8a6d259e4c16e1a95774f5c2384
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2022-02-13 23:03:56 +0900

    FIX - name 필드 최대 길이 수정 및 ip 주소 필드 추가

--- web/models.py
+++ web/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 
 
 class Message(models.Model):
-    name = models.CharField(max_length=8)
+    name = models.CharField(max_length=32)
     message = models.TextField()
+    ip_address = models.CharField(max_length=16)
     create_date = models.DateTimeField(default=timezone.now)

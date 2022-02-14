c9bd3fb1acc40d433787cbb9efe8bfdfc4ad698d
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2022-02-14 20:55:45 +0900

    FEAT - ip 주소 저장 구현

--- web/views.py
+++ web/views.py
@@ -4,10 +4,22 @@ from forms import MessageForm
 from web.models import Message
 
 
+def _get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
 def leave_message(request):
     if request.method == 'POST':
         form = MessageForm(request.POST)
         if form.is_valid():
+            form = form.save(commit=False)
+            ip = _get_client_ip(request)
+            form.ip_address = ip
             form.save()
             return redirect('web:message_list')
     else:

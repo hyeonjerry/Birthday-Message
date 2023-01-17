7d94a73c5f660d6a0febfb9b37e5df7b23d3d742
Hyeonjae-K <href.hjk@gmail.com>
2023-01-18 04:01:37 +0900

    fix: path 수정

--- backend/domain/birthday/birthday_router.py
+++ backend/domain/birthday/birthday_router.py
@@ -8,7 +8,7 @@ from domain.birthday import birthday_schema, birthday_crud
 router = APIRouter(prefix="/api/birthday")
 
 
-@router.get("detail/{birthday_id}", response_model=birthday_schema.Birthday)
+@router.get("/detail/{birthday_id}", response_model=birthday_schema.Birthday)
 def birthday_detail(birthday_id: str, db: Session = Depends(get_db)):
     birthday = birthday_crud.get_birthday(db, birthday_id)
     return birthday

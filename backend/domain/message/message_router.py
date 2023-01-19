94cd58b74d0915e5dacc90647f787d77100d3d6e
Hyeonjae-K <href.hjk@gmail.com>
2023-01-19 15:59:01 +0900

    fix: ip 주소 저장 제거

--- backend/domain/message/message_router.py
+++ backend/domain/message/message_router.py
@@ -15,11 +15,9 @@ router = APIRouter(
 @router.post("/create/{birthday_id}", status_code=status.HTTP_204_NO_CONTENT)
 def message_create(birthday_id: str,
                    _message_create: message_schema.MessageCreate,
-                   request: Request,
                    db: Session = Depends(get_db)):
     birthday = birthday_crud.get_birthday(db, birthday_id)
-    ip_addr = request.client.host
     if not birthday:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="데이터를 찾을 수 없습니다.")
-    message_crud.create_message(db, birthday, _message_create, ip_addr)
+    message_crud.create_message(db, birthday, _message_create)
